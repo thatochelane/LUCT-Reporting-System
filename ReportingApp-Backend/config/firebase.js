7a28f709b77474d885e24e116addb3e5e818dab4
thatochelane <thatochelane9@gmail.com>
2026-04-12 22:37:30 +0200

    Firebase config for hosting

--- ReportingApp-Backend/config/firebase.js
+++ ReportingApp-Backend/config/firebase.js
@@ -1,9 +1,16 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../serviceAccountKey.json');
+require('dotenv').config();
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('../serviceAccountKey.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
 });
 
 const db = admin.firestore();
